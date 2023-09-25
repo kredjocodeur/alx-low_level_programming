f1e8ccd5872c662811f81b2f3e1b2865a0c6086b
kredjocodeur <kredjocodeur@gmail.com>
2023-09-24 17:13:38 -0700

    Hello programming task 3 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
- * main : the enter point
+/**
+ * main : The enter point
+ *
  * The function return always 0
- * */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
