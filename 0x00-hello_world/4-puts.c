c594a53ce90b439c3e94bdd028eb4389fcbb863f
kredjocodeur <kredjocodeur@gmail.com>
2023-09-24 17:26:26 -0700

    Hello programming task 3 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: 'the program's description'
- * @parameter: describe the parameter
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
