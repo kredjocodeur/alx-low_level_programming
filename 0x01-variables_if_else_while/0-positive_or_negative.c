3d20f93614f09fe9cf015157005aec0908e68121
kredjocodeur <kredjocodeur@gmail.com>
2023-09-25 00:09:52 -0700

    Variables, if, else, while task 0 fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /**
  * main: Entry point
  *
- * Description: print the value of n status; zero, positive or negative
+ * Description: print the value of n satus; zero, positive or negative
  *
  * Return: The function return always 0
 */
